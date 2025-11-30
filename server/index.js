a93f557488e48f80a26b5e915b75f09d0613edc9
Satyamrk <satyamrk18@gmail.com>
2025-11-30 13:27:38 +0530

    server setup complete

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen("8080", (req, res) => {
+  console.log("server is running");
+});
